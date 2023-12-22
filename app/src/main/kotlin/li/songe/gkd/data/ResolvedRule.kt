2d03b8b518d05d4623c9057859a23fa3ac51419d
jiuqianyuan <39406781+jiuqianyuan@users.noreply.github.com>
2023-12-23 04:14:08 +0800

    Update ResolvedRule.kt

--- app/src/main/kotlin/li/songe/gkd/data/ResolvedRule.kt
+++ app/src/main/kotlin/li/songe/gkd/data/ResolvedRule.kt
@@ -107,8 +107,8 @@ sealed class ResolvedRule(
                 }
             }
             val t = System.currentTimeMillis()
-            LogUtils.d("t": t)
-            LogUtils.d("actionTriggerTime.value": actionTriggerTime.value)
+            LogUtils.d("t:",t)
+            LogUtils.d("actionTriggerTime.value:",actionTriggerTime.value)
             if (matchDelay > 0 && t - matchChangedTime < matchDelay) {
                 return 4 // 处于匹配延迟中
             }
